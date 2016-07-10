d80e7cd6d753708f6084d012fd4289c01fb1f28b
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2016-07-10 17:36:58 -0400

    Allow setting up different target_urls for a short url

--- url_shortener/database.py
+++ url_shortener/database.py
@@ -8,10 +8,26 @@ class Database():
     def __init__(self):
         self.session = create_session(DATABASE_URL)
 
-    def get_link(self, key):
-        return self.session.query(ShortUrl) \
-               .filter(ShortUrl.key==key) \
-               .first()
+    def get_links_with_clicks(self, key):
+        links = self.session.query(ShortUrl) \
+                .filter(ShortUrl.key==key) \
+                .all()
+        link_ids = [link.id for link in links]
+        click_query = self.session.query(Click) \
+                      .filter(Click.short_url_id.in_(link_ids)) \
+                      .order_by(Click.created_at)
+
+        results = []
+        for link in links:
+            query = click_query.filter(Click.short_url_id==link.id)
+            click_total = query.count()
+            clicks      = query.all()
+            results.append( {
+                'link': link,
+                'click_total': click_total,
+                #clicks: clicks
+            } )
+        return results
 
     def find_or_create_short_url(self, target_url):
         link = self.session.query(ShortUrl) \
@@ -29,15 +45,25 @@ class Database():
 
         return link
 
-    def save_click(self, link_id):
-        click = Click(short_url_id=link_id)
+    def save_click(self, link_id, referrer_url):
+        click = Click(
+            short_url_id=link_id,
+            referrer_url=referrer_url
+        )
         self.session.add(click)
         self.session.commit()
 
-    def query_clicks(self, link_id):
-        click_query = self.session.query(Click) \
-                      .filter(Click.short_url_id==link_id) \
-                      .order_by(Click.created_at)
-        click_total = click_query.count()
-        clicks      = click_query.all()
+    def query_clicks(self, link_ids):
+
         return [ click_total, clicks ]
+
+    def add_target(self, key, target_url, device_type):
+        link = ShortUrl(
+            key=key,
+            target_url=target_url,
+            user_agent='device_type:' + device_type
+        )
+        self.session.add(link)
+        self.session.commit()
+        return link
+
--- url_shortener/views.py
+++ url_shortener/views.py
@@ -1,15 +1,15 @@
 from flask import render_template, redirect, request
+import logging
 from url_shortener import app
-from sqlalchemy import create_engine
 from models import ShortUrl, Click
 import config
 from application import RegexConverter, create_session
 from database import Database
 
+
 app.url_map.converters['regex'] = RegexConverter
 db_session = create_session(config.DATABASE_URL)
 db = Database()
-
 
 @app.route('/')
 def index():
@@ -17,19 +17,27 @@ def index():
 
 @app.route('/shorten', methods=['POST'])
 def shorten():
-    target_url = request.form.get('url')
-    link       = db.find_or_create_short_url(target_url)
+    target_url  = request.form.get('url')
+    key         = request.form.get('key')
+    device_type = request.form.get('device_type')
+
+    if key == None:
+        link = db.find_or_create_short_url(target_url)
+    else:
+        link = db.add_target(key, target_url, device_type)
+
     short_url  = config.DOMAIN + link.key
     return render_template('link.html', link=link, short_url=short_url)
 
 @app.route('/<regex("[a-z0-9]{8}"):key>/')
 def redirect_to_target(key):
     link = db.get_link(key)
+    user_agent = request.user_agent
 
     if link == None:
         return render_template('404.html')
     else:
-        db.save_click(link.id)
+        db.save_click(link.id, request.referrer)
         return redirect(link.target_url)
 
 @app.route('/links')
@@ -40,10 +48,6 @@ def links():
 @app.route('/link/<regex("[a-z0-9]{8}"):key>/')
 def show_link(key):
     short_url = config.DOMAIN + key
-    link      = db.get_link(key)
-    result    = db.query_clicks(link.id)
+    results   = db.get_links_with_clicks(key)
 
-    return render_template(
-               'link.html', link=link, short_url=short_url,
-               click_total=result[0], clicks=result[1]
-           )
+    return render_template( 'link.html', results=results, short_url=short_url )
