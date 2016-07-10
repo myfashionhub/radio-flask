85fad3b4714aeb1cf0e384ef0403f279116b7d6c
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2016-07-10 12:08:55 -0400

    Save clicks on short links

--- url_shortener/views.py
+++ url_shortener/views.py
@@ -27,22 +27,8 @@ def redirect_to_target(key):
     if link == None:
         return render_template('404.html')
     else:
-        # update clicks
+        save_click(link)
         return redirect(link.target_url)
-
-def find_or_create_short_url(target_url):
-    link = session.query(ShortUrl).filter(ShortUrl.target_url==target_url).first()
-
-    if link == None:
-        key = str(uuid.uuid4())[:8]
-        link = ShortUrl(
-          key=key,
-          target_url=target_url
-        )
-        session.add(link)
-        session.commit()
-
-    return link
 
 @app.route('/links')
 def links():
@@ -53,7 +39,38 @@ def links():
 
 @app.route('/link/<regex("[a-z0-9]{8}"):key>/')
 def show_link(key):
-    link = session.query(ShortUrl).filter(ShortUrl.key==key).first()
-    short_url = config.DOMAIN + link.key
-    # Find number of clicks
-    return render_template('link.html', link=link, short_url=short_url)
+    short_url = config.DOMAIN + key
+    link      = session.query(ShortUrl) \
+                .filter(ShortUrl.key==key) \
+                .first()
+    click_query = session.query(Click) \
+                  .filter(Click.short_url_id==link.id) \
+                  .order_by(Click.created_at)
+    click_total = click_query.count()
+    clicks      = click_query.all()
+
+    return render_template(
+               'link.html', link=link, short_url=short_url,
+               click_total=click_total, clicks=clicks
+           )
+
+def find_or_create_short_url(target_url):
+    link = session.query(ShortUrl) \
+           .filter(ShortUrl.target_url==target_url) \
+           .first()
+
+    if link == None:
+        key = str(uuid.uuid4())[:8]
+        link = ShortUrl(
+            key=key,
+            target_url=target_url
+        )
+        session.add(link)
+        session.commit()
+
+    return link
+
+def save_click(link):
+    click = Click(short_url_id=link.id)
+    session.add(click)
+    session.commit()
