d936c0edd013fd38e5c7616de955620f69fd2be8
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2016-07-10 20:40:09 -0400

    Add debug logging mode

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 import url_shortener
+import logging
+from url_shortener import config
 
 if __name__ == '__main__':
-  url_shortener.app.run(debug=True)
+    if config.DEBUG_MODE == True:
+        logging.basicConfig(level=logging.DEBUG)
+
+    url_shortener.app.run(host='0.0.0.0', debug=config.DEBUG_MODE)
